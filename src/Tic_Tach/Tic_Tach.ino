e1b5f2a8f671278756eb3b315f0bab8682f0dd85
allan adams <awa@mit.edu>
2019-09-10 19:20:19 -0400

    Added Tic-Tach Commenting

--- src/Tic_Tach/Tic_Tach.ino
+++ src/Tic_Tach/Tic_Tach.ino
@@ -1,12 +1,32 @@
-// -------------------------------------------------------------
-// Simple flowmeter system with Teensy and CAN-bus
-// Aug 29th 2019
-// Authors: Junsu Jang, Allan Adams
-//
-// Description: 
-// Teensy uses Can0 (pin3(TX) and pin4(RX)) for communication
-// It waits for a command from the master (RESET or SET) and
-// at every sampling rate transmits a message as its heartbeat
+/* -------------------------------------------------------------
+ *   Tic-Tach: a Teensy 3.5 based flowmeter for the Mesobot
+ *   Sept 10, 2019
+ *   Authors: Junsu Jang, Allan Adams, FOL/MIT & WHOI
+ *
+ *      Description: 
+ *   
+ *   Tic-Tach is a Teensy 3.5 based flow-meter widget for the Mesobot's 
+ *   eDNA sampler system.   This firmware has three jobs: 
+ *   
+ *   1. Detect asynchronous pulses from two hall-switch-based flow 
+ *      sensors on Mesobot's eDNA sampler (open-collectors pulled up 
+ *      with a 15k resistor and filtered with a 300pf cap) and
+ *      update the appropriate counter.
+ * 
+ *   2. Output the two counters over CAN-bus once per sampling_period
+ * 
+ *   3. Wait on commands via CAN-bus:
+ *     A. RESET     Resets both counters
+ *     B. SET nnn   Sets hearbeat period to nnnn in us     
+ * 
+ *   Jobs 1 and 2 are implemented via interrupts:
+ *     1. rising-edge on appropriate pin
+ *     2. timer interrupt
+ * 
+ *   Job 3 is processed in the main loop.    
+ *   
+*/
+
 
 #include <FlexCAN.h>
 
@@ -21,14 +41,14 @@
 // Uncomment to see serial  data
 #define SERIAL_DUMP 1
 
-
-
+// Comment Out to get Little Endian; we want BE  for the network side
+#define BIG_ENDIAN_OUT 1
 
 // Define global variables
 IntervalTimer fm_timer;         // flowmeter timer instance
-static CAN_message_t out_msg;   // message instance for tx
-static uint32_t D1 = 16;
-static uint32_t D2 = 39;
+static CAN_message_t out_msg;   // FlexCANmessage instance for tx
+static uint32_t D1 = 16;        // FlowSensor 1 data line wired to Pin D1
+static uint32_t D2 = 39;        // FlowSensor 2 data line wired to Pin D2
 
 // Initialize shared variables
 uint32_t sampling_period = DEFAULT_SR;
@@ -41,6 +61,17 @@ volatile uint32_t sample2 = 0;
 volatile uint8_t fSendData = 0;  // flag for new measurement
 
 
+
+
+/***********************************************************
+ *       
+ *       isr_flowmeter1(), isr_flowmeter2()
+ *       
+ *   0. Triggered on GPIO Rising Edge Interrupt
+ *   1. Iterate the counter
+ *   2. Return  as  quickly as possible!
+ *       
+ */
 
 static void isr_flowmeter1()
 {
@@ -60,12 +91,20 @@ static void isr_flowmeter2() {
 
 
 
-// -------------------------------------------------------------
+
+/***********************************************************      
+ *       heartbeat_interrupt()
+ *       
+ *   0. Triggered on Timer
+ *   1. Sample and store the volatile stored counter values 
+ *   2. Set the new measurement flag to trigget ouput in the main loop
+ * 
+ *   NB: Pause interrupts while sampling counters to avoid collision with data interrupts
+ *       Dont forget to turn them back on at the end.
+ *       
+ */
 static void heartbeat_interrupt()
-{
-  // 1. Sample the flow and store the value
-  // 2. set the new measurement flag
-  
+{  
   cli();
   sample1 = flow_counter1;
   sample2 = flow_counter2;
@@ -83,7 +122,18 @@ Serial.println(sample2);
 
 
 
-// -------------------------------------------------------------
+
+/***********************************************************
+ * 
+ *     setup()
+ * 
+ *   Setup GPIO Pins
+ *   Initialize GPIO interrupts
+ *   Initialize CAN-bus interface
+ *   Initialize timer interrupt
+ *   
+ * 
+ */
 void setup(void)
 {
   delay(1000);
@@ -91,10 +141,12 @@ void setup(void)
   Serial.println(F("Hello Mesobot!!"));  
 #endif
 
-  // Setup data GPIO Pins
+  // Setup data GPIO Pins --  pull-up resistors (and filtering caps) present in hardware
   pinMode(D1, INPUT);
-  attachInterrupt(D1, isr_flowmeter1, RISING);
   pinMode(D2, INPUT);
+
+  // Attach interrupts to rising edges on the GPIO data lines
+  attachInterrupt(D1, isr_flowmeter1, RISING);
   attachInterrupt(D2, isr_flowmeter2, RISING);
 
   // Initialize the CAN-bus instance for communication
@@ -114,22 +166,23 @@ void setup(void)
   fm_timer.begin(heartbeat_interrupt, sampling_period);
 
   delay(1000);
-//    flow_counter1 = 4097;
-//    flow_counter2 = 0xFFF00F00;
   flow_counter1 = 0;
   flow_counter2 = 0;
 }
 
 
 
-// -------------------------------------------------------------
-static void handle_cmd(uint8_t *cmdPtr)
-{
-  // I assume the following:
+/***********************************************************
+ * 
+ *        handle_cmd(uint8_t*)
+ * 
   // First byte of the message contains the command 
   // next 4bytes of the message contains the relevant parameter values
   // (i.e. sampling rate in microseconds)
-  
+ *    
+ */
+static void handle_cmd(uint8_t *cmdPtr)
+{
   uint8_t cmd = cmdPtr[0];
 #ifdef SERIAL_DUMP
   Serial.println(cmd);
@@ -187,35 +240,16 @@ void loop(void)
     // New counter is available, transmit as heartbeat    
     for (int i = 0; i < 4; i++)
     {
-      //  Big-endian
+#ifdef BIG_ENDIAN_OUT
+    //  Big-Endian:  Byte 0 is MSB
+      out_msg.buf[3-i] = (sample1 >> (8*i)) & 0xFF;
+      out_msg.buf[7-i] = (sample2 >> (8*i)) & 0xFF;
+#else
+    // Little-Endian: Byte 0 is LSB
       out_msg.buf[i] = (sample1 >> (8*i)) & 0xFF;
       out_msg.buf[i+4] = (sample2 >> (8*i)) & 0xFF;      
-      // Little-Endian
-//      out_msg.buf[3-i] = (sample1 >> (8*i)) & 0xFF;
-//      out_msg.buf[7-i] = (sample2 >> (8*i)) & 0xFF;
-    }
-/*
-#ifdef SERIAL_DUMP
-    uint32_t o0 = out_msg.buf[0] ;
-    uint32_t o1 = out_msg.buf[1] ;
-    uint32_t o2 = out_msg.buf[2] ;
-    uint32_t o3 = out_msg.buf[3] ;
-    uint32_t o4 = out_msg.buf[4] ;
-    uint32_t o5 = out_msg.buf[5] ;
-    uint32_t o6 = out_msg.buf[6] ;
-    uint32_t o7 = out_msg.buf[7] ;
-    Serial.println(sample1);
-    Serial.println(o0);
-    Serial.println(o1);
-    Serial.println(o2);
-    Serial.println(o3);
-    Serial.println(sample2);
-    Serial.println(o4);
-    Serial.println(o5);
-    Serial.println(o6);
-    Serial.println(o7);
 #endif
-*/
+    }
     Can0.write(out_msg);
     // Reset the new measurement flag
     fSendData = 0;
